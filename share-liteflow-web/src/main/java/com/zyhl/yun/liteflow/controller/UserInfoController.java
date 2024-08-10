1a87fd659310f2332c3a01892d2bf91ca037f40f
WoodsWind <2553883726@qq.com>
2024-08-10 16:56:04 +0800

    为什么端口会被覆盖

--- share-liteflow-web/src/main/java/com/zyhl/yun/liteflow/controller/UserInfoController.java
+++ share-liteflow-web/src/main/java/com/zyhl/yun/liteflow/controller/UserInfoController.java
@@ -1,6 +1,7 @@
 package com.zyhl.yun.liteflow.controller;
 
 import com.zyhl.hcy.commons.result.BaseResult;
+import com.zyhl.yun.liteflow.domain.entity.UserDomainEntity;
 import com.zyhl.yun.liteflow.external.client.req.UserInfoReq;
 import com.zyhl.yun.liteflow.external.client.resp.UserInfoRsp;
 import org.springframework.validation.annotation.Validated;
@@ -10,12 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/yun/user")
 public class UserInfoController {
 
-    @PostMapping("/getUser")
-    public BaseResult<String> getUser(@RequestBody @Validated UserInfoReq req) {
-        String rsp = "这是一个回复";
+    @PostMapping("/yun/user/getUser")
+    public BaseResult<UserInfoRsp> getUser(@RequestBody @Validated UserInfoReq req) {
+        UserInfoRsp rsp = new UserInfoRsp();
+        rsp.setData(new UserDomainEntity(111111L,"ccc"));
         return BaseResult.success(rsp);
     }
 }
